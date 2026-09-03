cbc1b1140b534563cb6e191e874f53aaec451a2c
Gorills <57192423+Gorills@users.noreply.github.com>
2026-09-03 14:58:35 +0700

    Expose uncaught world test exceptions

--- tests/test_world.cpp
+++ tests/test_world.cpp
@@ -1,3 +1,20 @@
+#define main worldsim_test_main
 #include "test_world_part1.inc"
 #include "test_world_part2.inc"
 #include "test_world_part3.inc"
+#undef main
+
+#include <exception>
+#include <iostream>
+
+int main() {
+    try {
+        return worldsim_test_main();
+    } catch (const std::exception& e) {
+        std::cerr << "UNCAUGHT worldsim_tests exception: " << e.what() << '\n';
+        return 2;
+    } catch (...) {
+        std::cerr << "UNCAUGHT worldsim_tests non-standard exception\n";
+        return 3;
+    }
+}
