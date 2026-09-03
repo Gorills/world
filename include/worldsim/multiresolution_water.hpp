2a6f09401e7a20978c3e0ea69df40cad2bbd26fc
Gorills <57192423+Gorills@users.noreply.github.com>
2026-09-03 13:42:27 +0700

    Expose cached coarse soil metadata internally

--- include/worldsim/multiresolution_water.hpp
+++ include/worldsim/multiresolution_water.hpp
@@ -42,6 +42,7 @@ private:
 
     [[nodiscard]] ContinentalWaterCellState& coarse_cell_mutable(std::size_t index) noexcept;
     [[nodiscard]] double coarse_area_m2(std::size_t index) const noexcept;
+    [[nodiscard]] SoilProperties coarse_soil_properties(std::size_t index) const noexcept;
     [[nodiscard]] std::uint32_t coarse_downstream_index(std::size_t index) const noexcept;
     [[nodiscard]] bool coarse_is_ocean(std::size_t index) const noexcept;
     [[nodiscard]] const std::vector<std::uint32_t>& coarse_routing_order() const noexcept;
